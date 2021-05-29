e07ec68c770d1961b86c98aa8a8b7f164c480c4a
Katia Fernandes dos Anjos <kfanjos86@gmail.com>
2021-05-28 21:33:10 -0300

    Update Poem.py

--- Poem.py
+++ Poem.py
@@ -1,1 +1,16 @@
 
+#imprimir      O tempo Voa            .
+print("     O tempo Voa            ")
+
+print("TIME    Passes     BY.")
+print("TIME    Passes     BY.")
+print("TIME    Passes     BY.")
+print("TIME    Passes     BY.")
+print("I       Pass       BY.")
+print("                   BY.")
+print("                   BY.")
+print("                   BYE.")
+
+
+print("                                                     Anatol Knotek.")
+#imprimir                                                     Anatol Knotek.
